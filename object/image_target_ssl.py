effee33665d3e9045f154f0912eeada67140f0da
ghostchoir <ideal_kar@snu.ac.kr>
2022-04-26 01:00:44 +0900

    can use wa for cls dis loss

--- object/image_target_ssl.py
+++ object/image_target_ssl.py
@@ -364,7 +364,7 @@ def train_target(args):
             netH.train()
             netB.train()
 
-            if args.cls_discrepancy_weight > 0:
+            if args.cls_dis_weight > 0:
                 centroids = torch.from_numpy(centroids).float().cuda()
                 centroids.requires_grad = False
                 centroids = F.normalize(centroids, dim=1)
@@ -478,11 +478,18 @@ def train_target(args):
         else:
             classifier_loss = torch.tensor(0.0).cuda()
 
-        if iter_num % args.cls_discrepancy_freq == 0 and args.cls_discrepancy_weight > 0 and not args.cls_discrepancy_c_update:
-            with torch.set_grad_enabled(not args.freeze_ccc):
-                c_ccc = F.normalize(b1, dim=1) @ centroids.T / args.ccc_temp
+        if iter_num % args.cls_dis_freq == 0 and args.cls_dis_weight > 0 and not args.cls_dis_c_update:
+            if args.wa_on_cls_dis:
+                b = b1
+                c = outputs_test
+            else:
+                b = b3
+                c = c3
 
-            classifier_loss += args.cls_discrepancy_weight * JSDivLoss(reduction='sum')(outputs_test, c_ccc)
+            with torch.set_grad_enabled(not args.freeze_ccc):
+                c_ccc = F.normalize(b, dim=1) @ centroids.T / args.ccc_temp
+
+            classifier_loss += args.cls_dis_weight * JSDivLoss(reduction='sum')(c, c_ccc)
 
         if args.ent_par > 0:
             softmax_out = nn.Softmax(dim=1)(outputs_test)
@@ -559,13 +566,16 @@ def train_target(args):
         classifier_loss.backward()
         optimizer.step()
 
-        if iter_num % args.cls_discrepancy_freq == 0 and args.cls_discrepancy_weight > 0 and args.cls_discrepancy_c_update:
-            b1 = netB(netF(inputs_test1))
+        if iter_num % args.cls_dis_freq == 0 and args.cls_dis_weight > 0 and args.cls_dis_c_update:
+            if args.wa_on_cls_dis:
+                b1 = netB(netF(inputs_test3))
+            else:
+                b1 = netB(netF(inputs_test1))
             c1 = netC(b1, labels_forward)
             with torch.set_grad_enabled(not args.freeze_ccc):
                 c_ccc = F.normalize(b1, dim=1) @ centroids.T / args.ccc_temp
 
-            cls_dis_loss = args.cls_discrepancy_weight * JSDivLoss(reduction='sum')(c1, c_ccc)
+            cls_dis_loss = args.cls_dis_weight * JSDivLoss(reduction='sum')(c1, c_ccc)
             c_optimizer.zero_grad()
             cls_dis_loss.backward()
             c_optimizer.step()
@@ -942,9 +952,11 @@ if __name__ == "__main__":
     parser.add_argument('--cr_scheduling', type=str, choices=['const', 'linear', 'step'], default='const')
     parser.add_argument('--cls_scheduling', type=str, choices=['const', 'linear', 'step'], default='const')
 
-    parser.add_argument('--cls_discrepancy_weight', type=float, default=0.0)
-    parser.add_argument('--cls_discrepancy_c_update', type=str2bool, default=False)
-    parser.add_argument('--cls_discrepancy_freq', type=int, default=100)
+    parser.add_argument('--cls_dis_weight', type=float, default=0.0)
+    parser.add_argument('--cls_dis_c_update', type=str2bool, default=False)
+    parser.add_argument('--cls_dis_freq', type=int, default=100)
+    parser.add_argument('--wa_on_cls_dis', type=str2bool, default=True)
+
     parser.add_argument('--freeze_ccc', type=str2bool, default=True)
     parser.add_argument('--ccc_temp', type=float, default=1.0)
 
