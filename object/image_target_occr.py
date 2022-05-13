65cc33348898c4d21f934c36cd58e8fed163fd29
ghostchoir <ideal_kar@snu.ac.kr>
2022-05-13 17:12:19 +0900

    evolving label mutation prob

--- object/image_target_occr.py
+++ object/image_target_occr.py
@@ -337,6 +337,7 @@ def train_target(args):
             netH.eval()
             netB.eval()
 
+            args.label_mutate_p = max(0, args.label_mutate_p + args.mutate_p_delta)
             c, p, f, o = get_outputs(dset_loaders['cal'], netF, netH, netB, netC)
             hc_idxs = get_topk_conf_indices(c, p, n_classes=args.class_num, k=args.hc_topk, threshold=args.hc_threshold)
 
@@ -1101,6 +1102,7 @@ if __name__ == "__main__":
     parser.add_argument('--batch_threshold', type=float, default=0.8)
     parser.add_argument('--full_logging', type=str2bool, default=True)
     parser.add_argument('--label_mutate_p', type=float, default=0.0)
+    parser.add_argument('--mutate_p_delta', type=float, default=-0.1)
     parser.add_argument('--mutate_mode', type=str, default='dl', choices=['dl', 'd', 'dal'])
     parser.add_argument('--minent_mode', type=str, default='all', choices=['all', 'a', 'ah', 'h'])
     parser.add_argument('--memax_mode', type=str, default='all', choices=['all', 'd', 'dl', 'l'])
