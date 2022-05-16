3c0fa9960929e95b378aa7e38bb18f661830a747
ghostchoir <ideal_kar@snu.ac.kr>
2022-05-16 21:53:16 +0900

    configurable cr reduction

--- object/image_target_occr.py
+++ object/image_target_occr.py
@@ -278,11 +278,11 @@ def train_target(args):
         elif args.cr_metric == 'l2':
             dist = nn.PairwiseDistance(p=2)
         elif args.cr_metric == 'bce':
-            dist = nn.BCEWithLogitsLoss(reduction='sum').cuda()
+            dist = nn.BCEWithLogitsLoss(reduction=args.cr_reduction).cuda()
         elif args.cr_metric == 'kl':
-            dist = nn.KLDivLoss(reduction='sum').cuda()
+            dist = nn.KLDivLoss(reduction=args.cr_reduction).cuda()
         elif args.cr_metric == 'js':
-            dist = JSDivLoss(reduction='sum').cuda()
+            dist = JSDivLoss(reduction=args.cr_reduction).cuda()
 
     optimizer = optim.SGD(param_group)
     optimizer = op_copy(optimizer)
@@ -1068,6 +1068,7 @@ if __name__ == "__main__":
     parser.add_argument('--cr_metric', type=str, default='cos', choices=['cos', 'l1', 'l2', 'bce', 'kl', 'js'])
     parser.add_argument('--cr_site', type=str, default='btn', choices=['feat', 'btn', 'cls'])
     parser.add_argument('--cr_threshold', type=float, default=0.0)
+    parser.add_argument('--cr_reduction', type=str, default='sum', choices=['sum', 'mean'])
     parser.add_argument('--angular_temp', type=float, default=0.1)
     parser.add_argument('--conf_threshold', type=float, default=0)
     parser.add_argument('--paws_weight', type=float, default=0.0)
